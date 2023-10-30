fa8b42fe6f8498f7ca2998285bf6d3cee6ae80c1
JugglypuffM <stas.klinin@gmail.com>
2023-10-30 22:30:42 +0500

    Requested Features
    Slight changes in the User and UserStorage class.

--- src/test/java/mainBot/UserStorageTests.java
+++ src/test/java/mainBot/UserStorageTests.java
@@ -7,17 +7,17 @@ public class UserStorageTests {
     @Test
     public void addUserTest(){
         UserStorage storage = new UserStorage();
-        Assertions.assertFalse(storage.getUserList().containsKey("0"));
+        Assertions.assertFalse(storage.getUserDict().containsKey("0"));
         storage.addUser("0");
-        Assertions.assertTrue(storage.getUserList().containsKey("0"));
+        Assertions.assertTrue(storage.getUserDict().containsKey("0"));
     }
     @Test
     public void getUserTest(){
         UserStorage storage = new UserStorage();
         storage.addUser("0");
         //Does return exact same user
-        storage.getUserList().get("0").setAge(12);
-        User A = storage.getUserList().get("0");
+        storage.getUserDict().get("0").setAge(12);
+        User A = storage.getUserDict().get("0");
         Assertions.assertEquals(storage.getUser("0").getAge(), A.getAge());
         //Does return null if non-existing id given
         Assertions.assertNull(storage.getUser("1"));
