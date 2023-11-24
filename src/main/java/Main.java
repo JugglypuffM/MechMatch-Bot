e2f2d0f8b10c956462f1062aa2a65be7b5b85d43
JugglypuffM <stas.klinin@gmail.com>
2023-11-24 21:51:51 +0500

    Logging with slf4j-log4j add
    Logs store at /logs/logs.log dir, which was added to .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
+import org.apache.log4j.*;
 import telegrammBot.BotRegistrar;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.INFO);
+        try {
+            Logger.getRootLogger().addAppender(new FileAppender(new SimpleLayout(), "./logs/logs.log"));
+        }catch (IOException e){
+            System.out.println("Failed to initialize logger.");
+            e.printStackTrace(System.out);
+        }
         BotRegistrar registrar = new BotRegistrar();
         registrar.start();
     }
