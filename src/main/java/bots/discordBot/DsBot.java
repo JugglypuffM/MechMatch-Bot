91c823e41d456f7a931a41978373b1230e44a8e3
JugglypuffM <stas.klinin@gmail.com>
2023-12-01 16:35:49 +0500

    Discord bot integration

---
+++ src/main/java/bots/discordBot/DsBot.java
@@ -0,0 +1,91 @@
+package bots.discordBot;
+
+import bots.Bot;
+import bots.BotDriver;
+import bots.platforms.Platform;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.FileUpload;
+import org.jetbrains.annotations.NotNull;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+
+public class DsBot extends ListenerAdapter implements Bot {
+    private final Dotenv dotenv = Dotenv.load();
+    private final BotDriver driver;
+    private final JDA jda = JDABuilder.createDefault(dotenv.get("DS_BOT_TOKEN"))
+            .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+            .build();
+    public DsBot(BotDriver m_driver){
+        this.driver = m_driver;
+    }
+    public boolean start() {
+        try {
+            jda.addEventListener(new DsBot(this.driver));
+            jda.awaitReady();
+        } catch (InterruptedException e) {
+            driver.getLogger().error("Bot registration failed", e);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if(event.getAuthor().isBot()){
+            return;
+        }
+        String message = event.getMessage().getContentDisplay();
+        String id = event.getChannel().getId();
+        String username = event.getAuthor().getName();
+        List<Message.Attachment> attachments = event.getMessage().getAttachments();
+        String[] reply = (!attachments.isEmpty() && Objects.equals(attachments.get(0).getFileExtension(), "png")) ?
+                driver.handleUpdate(id, username, attachments.get(0).getUrl(), true) :
+                driver.handleUpdate(id, username, message, false);
+        driver.send(this, id, username, message, reply);
+    }
+
+
+    @Override
+    public boolean executePhoto(String id, String message, String photo) {
+        try {
+            PrivateChannel channel = jda.getPrivateChannelById(id);
+            if (channel == null) return false;
+            URL url = new URL(photo);
+            BufferedImage img = ImageIO.read(url);
+            File file = new File("./tmp.png");
+            ImageIO.write(img, "png", file);
+            channel.sendMessage(message).addFiles(FileUpload.fromData(file)).queue();
+            return true;
+        }catch (Exception e){
+            driver.getLogger().error("Failed to handle the image.", e);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean executeText(String id, String message) {
+        PrivateChannel channel = jda.getPrivateChannelById(id);
+        if (channel != null){
+            channel.sendMessage(message).queue();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Platform getPlatform() {
+        return Platform.DISCORD;
+    }
+}
